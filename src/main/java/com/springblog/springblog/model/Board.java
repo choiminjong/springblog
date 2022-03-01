f04d4667499464e1ca12f6f87e6de7c0c307b617
alswhd20 <alswhd20@gmail.com>
2022-03-01 13:59:02 +0900

    Spring Boot으로 웹 출시까지 #6. thymleaf에서 form 전송하기

--- src/main/java/com/springblog/springblog/model/Board.java
+++ src/main/java/com/springblog/springblog/model/Board.java
@@ -1,20 +1,29 @@
 package com.springblog.springblog.model;
 
+
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 @Data
 @Entity
 public class Board {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자이하로 작성해주세요")
     private String title;
+
     private String content;
 
 }
---
+++ src/main/java/com/springblog/springblog/validator/BoardValidator.java
@@ -0,0 +1,25 @@
+package com.springblog.springblog.validator;
+
+import antlr.StringUtils;
+import com.springblog.springblog.model.Board;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class BoardValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Board.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object obj, Errors errors) {
+        Board b = (Board) obj;
+        if(b.getContent()==""){
+            errors.rejectValue("content","key","내용을 입력하세요.");
+        }
+    }
+
+}
