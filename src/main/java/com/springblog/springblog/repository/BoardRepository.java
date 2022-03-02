886d39e32634254de2a1fb78ccf6d662025d3d3d
민종 최 <dominic@nexon.co.kr>
2022-03-02 15:20:17 +0900

    Spring Boot으로 웹 출시까지 #8. JPA를 이용한 페이지 처리 및 검색

--- src/main/java/com/springblog/springblog/repository/BoardRepository.java
+++ src/main/java/com/springblog/springblog/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.springblog.springblog.repository;
 
 import com.springblog.springblog.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,7 @@ public interface BoardRepository extends JpaRepository<Board,Long> {
 
     List<Board> findByTitle(String title);
     List<Board> findByTitleOrContent(String title,String content);
+
+    // Title , Content like 검색을 할 수 있다.
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
